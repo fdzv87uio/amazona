64ad085666e428a1d8570395a84e1dfba16df27f
@fausto1 <fausto@parts-badger.com>
2021-12-01 17:44:55 -0500

    chore: adding React context

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import {
@@ -10,11 +10,44 @@ import {
   Link,
   ThemeProvider,
   CssBaseline,
+  createMuiTheme,
+  Switch,
 } from '@material-ui/core';
-import { theme } from '../utils/theme';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const classes = useStyles();
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        margin: '1rem 0',
+        fontWeight: 'bolder',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        margin: '1rem 0',
+        fontWeight: 'bolder',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const darkModeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode === true ? 'ON' : 'OFF');
+  };
   return (
     <div>
       <Head>
@@ -33,6 +66,7 @@ function Layout({ title, description, children }) {
               </Link>
             </NextLink>
             <div className={classes.grow}></div>
+            <Switch checked={darkMode} onChange={darkModeHandler}></Switch>
             <NextLink href="/cart" passHref>
               <Link>
                 <Typography>Cart</Typography>
