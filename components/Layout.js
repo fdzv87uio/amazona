2c85f1e064e7931f43eb2b7583c31beb2aac37c8
@fausto1 <fausto@parts-badger.com>
2021-12-01 10:45:21 -0500

    chore: fix style rendering error

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Typography, Toolbar, Container } from '@material-ui/core';
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Container,
+  makeStyles,
+} from '@material-ui/core';
 
 function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Amazona</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <Typography>Amazona</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>2021 - All rights reserved</Typography>
       </footer>
     </div>
   );
 }
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      margin: 10,
+    },
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    textAlign: 'center',
+  },
+});
 
 export default Layout;
---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { ServerStyleSheets } from '@material-ui/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class Mydocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+Mydocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
